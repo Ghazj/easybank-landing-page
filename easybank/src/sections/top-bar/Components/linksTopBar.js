dfda9cfd660eade6d67306f701ee4d4763704fe9
Ghazj <ghazijj98@gmail.com>
2020-07-01 02:32:16 -0300

    Estilso a links de topBar

--- easybank/src/sections/top-bar/Components/linksTopBar.js
+++ easybank/src/sections/top-bar/Components/linksTopBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+let styles = 'height: 80px; background-color: transparent; border: 0;'
+
 function linksTopBar() {
     return (
         <div id="linksTopBar">
-            <button>Home</button>
-            <button>About</button>
-            <button>Contact</button>
-            <button>Blog</button>
-            <button>Carrers</button>
+            <button styles={styles}>Home</button>
+            <button styles={styles}>About</button>
+            <button styles={styles}>Contact</button>
+            <button styles={styles}>Blog</button>
+            <button styles={styles}>Carrers</button>
         </div>
     )
 }
